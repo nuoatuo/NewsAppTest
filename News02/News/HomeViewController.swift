3b409d5158ffc2174bccec601ecd758ba97f74b4
nuoatuo <nuoatuo@126.com>
2016-10-11 09:59:07 +0800

    发送网络请求
    发送网络请求

---
+++ News02/News/HomeViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HomeViewController.swift
+//  News
+//
+//  Created by 古今 on 16/10/10.
+//  Copyright © 2016年 夜风易冷. All rights reserved.
+//
+
+/*
+ 网络请求
+ OC -> AFNetworking
+ Swift -> Alamofire
+ Swift3.0 -> 4.0.0+  -> iOS9
+  Swift2.3 -> 3.5.0+  -> iOS8
+ */
+
+import UIKit
+// MARK: - 类的声明
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //1.设置导航栏
+        setupNavigationBar()
+    }
+    
+    /*
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+     */
+    
+}
+
+//类扩展
+//MARK: - 设置UI界面
+extension HomeViewController {
+    fileprivate func setupNavigationBar()  {
+        //1.设置背景图片
+        navigationController?.navigationBar.setBackgroundImage(UIImage(named: "reader_navigation_background"), for: .default)
+        
+        //2.设置标题
+        navigationItem.titleView = UIImageView(image: UIImage(named: "navigation_logo"))
+        
+        //3.设置右侧的搜索按钮
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "navigation_search"), style: .plain, target: self, action: #selector(searchItemClick))
+    }
+}
+
+//MARK: - 事件监听的函数
+extension HomeViewController {
+    //@objc ---> 为了保留OC的特性
+    @objc fileprivate func searchItemClick()  {
+        print("------")
+    }
+}
